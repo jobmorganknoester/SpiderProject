4cb0ee20ea207603d4185244f76f0d5aa007b385
jobmorganknoester <job.morgan.knoester@gmail.com>
2017-05-15 17:01:30 -0300

    Git initialization

---
+++ script.R
@@ -0,0 +1,41 @@
+# PCA plot unused function :
+
+pcaplot <- function (pca, firstaxis, secondaxis) {
+  
+  pcares <- cbind(pca$li,proportion$Plot,proportion$Transect) 
+  names(pcares)[c(dim(pca$li)[2]+1,dim(pca$li)[2]+2)] <- c("Plot","Transect")
+  eig <- pca$eig
+  names(eig) <- paste("Axis", c(1:length(eig)))
+  eig <- as.data.frame(eig)
+  
+  
+  plot <- ggplot() + 
+    theme_bw() + 
+    geom_vline(aes(xintercept=0), color="black", size=0.5) +
+    geom_hline(aes(yintercept=0), color="black", size=0.5) +
+    geom_segment(data=pca$co, aes(x = 0, xend = pca$co[,firstaxis]*10, y = 0, yend = pca$co[,secondaxis]*10), color="black", linetype = "dashed") +
+    geom_text(data=pca$co, aes(x=pca$co[,firstaxis]*10, y=pca$co[,secondaxis]*10, label=rownames(pca$co)), size = 3, color="black") +
+    geom_text(data=eig, aes(x=max(c(max(pcares[,firstaxis])+0.5,1.5)), y=0, label=paste("Axis",firstaxis,":",round(eig[firstaxis]/pca$rank,3)*100,"%")), size = 3, vjust=1, color="blue") +
+    geom_text(data=eig, aes(x=0, y=max(c(max(pcares[,secondaxis])+0.5,1.5)), label=paste("Axis",secondaxis,":",round(eig[secondaxis]/sum(eig),3)*100,"%")), size = 3, vjust=1, color="blue") +
+    geom_point(data=pcares, aes(x = pcares[,firstaxis], y =pcares[,secondaxis], color = Plot, shape = Transect), size = 1.5) +
+    scale_shape(solid = FALSE)
+  
+  return (ggplotly(plot))
+}
+
+pcaproportionlog <- dudi.pca(proportionlog[,c(10:15)], center=T, scale=T, scannf = F, nf = 4)
+barplot((pcaproportionlog$eig/length(pcaproportionlog$eig))*100)
+# pcaproportionlog$eig
+# pcaproportionlog$co
+
+pcaplot(pca = pcaproportionlog, firstaxis = 1, secondaxis = 2)
+pcaplot(pca = pcaproportionlog, firstaxis = 2, secondaxis = 3)
+pcaplot(pca = pcaproportionlog, firstaxis = 1, secondaxis = 3)
+pcaplot(pca = pcaproportionlog, firstaxis = 1, secondaxis = 4)
+pcaplot(pca = pcaproportionlog, firstaxis = 2, secondaxis = 4)
+pcaplot(pca = pcaproportionlog, firstaxis = 3, secondaxis = 4)
+
+
+
+
+
